01e7086405705816a29c97cd119d91a5f129da90
ruddropust <sharkabir01@gmail.com>
2025-01-16 10:48:22 +0600

    Complete video 7

---
+++ video7.js
@@ -0,0 +1,35 @@
+//Operation
+let value = 3;
+let negvalue = -value
+
+// console.log(negvalue)
+// console.log(2+2)
+// console.log(10-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2%2)
+// console.log(2**3)
+// console.log(2**0.5)
+// console.log(Math.pow(2,3))
+// console.log(Math.pow(2,0.5))
+
+//string opration
+let str = "hello"
+let str2 = "ruddro"
+let str3 = str + " " + str2
+// console.log(str3, typeof str3, str3.length)
+//problem on string
+// console.log(1+"2")
+// console.log("1"+2)
+//sreing
+// console.log("1"+2+2)  //check from left to right
+// console.log(1+2+"2")  //1+2 = 3 then 3+"2" = 32
+//number
+let num1,num2,num3
+num1 = num2 = num3 = 2+2
+// console.log(num1,num2,num3)
+console.log("-------------------")
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter)
