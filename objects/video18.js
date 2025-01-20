2bc2d189f1fda64b1c3f519589cb9a00d1f70267
ruddropust <sharkabir01@gmail.com>
2025-01-20 08:09:47 +0600

    json

---
+++ objects/video18.js
@@ -0,0 +1,35 @@
+// const course = {
+//     courseName: "JS Ease",
+//     price: 999,
+//     courseInstructor: "Habib",
+// }
+// //simplify to access object property
+// const {courseInstructor} = course
+// console.log(courseInstructor);
+
+// console.log("------------------------------");
+// //more simplify to access object property
+// const{courseInstructor: instructor} = course
+// console.log(instructor);
+
+
+//destructure a object
+const navbar = ({company}) => {
+
+}
+navbar (company="habib")
+
+//how json from
+
+// {
+//     "name": "Habib",
+//     "courseName": "Learning JS Easy",
+//     "Price": "free",
+// }
+//api array format
+// [
+//     {},
+//     {},
+//     {}
+// ]
+//json formattor for details about api
