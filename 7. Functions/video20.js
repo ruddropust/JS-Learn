421285aa92ccc95bd669ba99b52571f64a2ddd84
ruddropust <sharkabir01@gmail.com>
2025-03-26 01:12:51 +0600

    submit

--- 7. Functions/video20.js
+++ 7. Functions/video20.js
@@ -7,32 +7,32 @@
 
 function CalculatePrice(val1,val2,...num1) //res operator
 {
-    return num1
+    return val1,val2,num1
 }
 console.log(CalculatePrice(200,2001,300,100,200));
 
 
-const user = {
-    username : "Habib",
-    cartPrice: 2500,
-}
-function handleObject(anyobject)
-{
-    console.log(`Username is ${anyobject.username} and
-        cart price is ${anyobject.cartPrice}`);
+// const user = {
+//     username : "Habib",
+//     cartPrice: 2500,
+// }
+// function handleObject(anyobject)
+// {
+//     console.log(`Username is ${anyobject.username} and
+//         cart price is ${anyobject.cartPrice}`);
     
-}
+// }
 
-handleObject(user)
-handleObject({
-    username: "ruddro",
-    cartPrice: 1600
-})
+// handleObject(user)
+// handleObject({
+//     username: "ruddro",
+//     cartPrice: 1600
+// })
 
-const myNewArray=[200,400,100,600]
+// const myNewArray=[200,400,100,600]
 
-function returnSecondValue(getArray)
-{
-    return getArray[1]
-}
-console.log(returnSecondValue(myNewArray));
+// function returnSecondValue(getArray)
+// {
+//     return getArray[1]
+// }
+// console.log(returnSecondValue(myNewArray));
